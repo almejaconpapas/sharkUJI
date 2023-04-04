9c984e84e6f5da0485ed268770ac47b318d01f9e
minitomate <al396821@uji.es>
2023-04-04 16:57:54 +0200

    Initial commit

---
+++ app/src/main/java/com/al396821/sharkuij/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.al396821.sharkuij
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
